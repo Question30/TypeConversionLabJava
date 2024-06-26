567c1fa5e242ca93ba500e7e6c7c65862558d266
Alexander Gomez <alexgomezgomez15@gmail.com>
2024-06-26 12:08:02 -0400

    glab done

---
+++ src/AutoTypeConversion.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class AutoTypeConversion {
+    public static void main(String[] args) {
+        int x = 20;
+        double y = 40.5;
+
+        long p = 30;
+        float q = 10.60F;
+
+        double z = x + y;
+        System.out.println("Sum of two numbers: " + z);
+
+        float r = p -q;
+        System.out.println("Subtraction of the two numbers: " + r);
+    }
+}
